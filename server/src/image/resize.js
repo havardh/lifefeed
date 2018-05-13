3d06206747c8f87ab9a4d29fc738b67bf4c20007
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2018-05-13 11:07:08 +0200

    fix: make response wait on resizing operation

--- server/src/image/resize.js
+++ server/src/image/resize.js
@@ -6,39 +6,39 @@ import mkdirp from "mkdirp";
 
 const sizes = [512, 768, 1024, 1536, 2048];
 
-function resizeTo(size) {
-  return async ({path}) => {
-    try {
-      const inFile = path;
-      const outFile = join(process.cwd(), dirname(path), `${size}`, basename(path));
+async function resizeFile({path}, size) {
+  try {
+    const inFile = path;
+    const outFile = join(process.cwd(), dirname(path), `${size}`, basename(path));
 
-      await promisify(mkdirp)(dirname(outFile));
+    await promisify(mkdirp)(dirname(outFile));
+    try {
+      await promisify(access)(outFile, constants.F_OK);
+      console.log(`Already resized: ${outFile}`)
+    } catch (e) {
       try {
-        await promisify(access)(outFile, constants.F_OK);
-        console.log(`Already resized: ${outFile}`)
+        await promisify(access)(inFile, constants.F_OK);
+        await promisify(exec)(`convert ${inFile} -resize ${size} ${outFile}`);
+        console.log(`Resized: ${outFile}`);
       } catch (e) {
-        try {
-          await promisify(access)(inFile, constants.F_OK);
-          await promisify(exec)(`convert ${inFile} -resize ${size} ${outFile}`);
-          console.log(`Resized: ${outFile}`);
-        } catch (e) {
-          if (e.code === "ENOENT") {
-            console.error(`Could not find file to resize ${inFile}`, e);
-          } else {
-            throw e;
-          }
+        if (e.code === "ENOENT") {
+          console.error(`Could not find file to resize ${inFile}`, e);
+        } else {
+          throw e;
         }
       }
-    } catch (err) {
-      console.error(err);
-      throw err;
     }
+  } catch (err) {
+    console.error(err);
+    throw err;
   }
 }
 
 export async function resizeFiles(files) {
   for (let size of sizes) {
-    await files.map(resizeTo(size));
+    for (let file of files) {
+      await resizeFile(file, size);
+    }
   }
 }
 
